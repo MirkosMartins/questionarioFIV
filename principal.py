8ed60dd132c200d73f794b70d5179e7e0a6806db
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-12-06 14:06:02 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -24,6 +24,16 @@ dimensao3.selectbox('Information after treatment',("I received good informationr
                                                    "I received good informationregarding the time between embryo transfer and pregnancy test."))
 dimensao3.selectbox('Participation',("I had good opportunitiesto participate in the decisions that applied to my treatment",
                                      "My carewasdetermined bymyownrequestsneedsratherthanthe staff’s procedures."))
+dimensao3.selectbox('Responsibility/continuity',("I received good information about which doctor was responsible for my treatment.",
+                                                 "I received good information about whichmidwife was responsible for my treatment.",
+                                                 "I met the same doctor at examinations and treatment during this treatment period.",
+                                                 "I met the same midwife at examinations and interviews during this treatment period."))
+dimensao3.selectbox('The Staff’s respect/commitment/empathy',("The doctors were respectful towards me.",
+                                                              "The doctors showed commitment; ‘cared about me’.",
+                                                              "The doctors seemed to understand how I experienced my situation.",
+                                                              "The midwives/nurses/laboratory personnel were respectful towards me.",
+                                                              "The midwives/nurses/laboratory personnel showed commitment; ‘cared about me’.",
+                                                              "Themidwives/nurses/laboratory personnel seemedto understand how I experienced my situation."))
 dimensao4 = st.container()
 dimensao4.write('Dimensao 4 Atmosfera socio-cultural')
 dimensao4.selectbox('Atmosphere and environment',("I received the best possible medical care (examinations and treatments) as far as I can tell"))
