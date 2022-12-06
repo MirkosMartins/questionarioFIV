d2a5fef88c6e9ba5a0859c86a4409c0126e1dd72
Mirkos Martins <MirkosMartins@users.noreply.github.com>
2022-12-06 14:27:32 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -17,23 +17,25 @@ dimensao2.selectbox('Características da sala de atendimento',("Tive acesso a um
                                                  "Tive acesso a uma agradável sala de tratamento durante a aspiração de oócitos e transferência de embriões."))
 dimensao3 = st.container()
 dimensao3.write('Dimensão 3 Abordagem orientada para o auto-reconhecimento')
-dimensao3.selectbox('Information during treatment',("I received the best possible medical care (examinations and treatments) as far as I can tell",
-                                                   "I received good informationregarding the drugs I needed, so that I understood their effects, and how they should be administered.",
-                                                    "I received good information about results from examinations (for example ultrasound, hormone and sperm analyses)."))
-dimensao3.selectbox('Information after treatment',("I received good informationregarding the fertilization and embryo development at the time of embryo transfer.",
-                                                   "I received good informationregarding the time between embryo transfer and pregnancy test."))
-dimensao3.selectbox('Participation',("I had good opportunitiesto participate in the decisions that applied to my treatment",
-                                     "My carewasdetermined bymyownrequestsneedsratherthanthe staff’s procedures."))
-dimensao3.selectbox('Responsibility/continuity',("I received good information about which doctor was responsible for my treatment.",
-                                                 "I received good information about whichmidwife was responsible for my treatment.",
-                                                 "I met the same doctor at examinations and treatment during this treatment period.",
-                                                 "I met the same midwife at examinations and interviews during this treatment period."))
-dimensao3.selectbox('The Staff’s respect/commitment/empathy',("The doctors were respectful towards me.",
-                                                              "The doctors showed commitment; ‘cared about me’.",
-                                                              "The doctors seemed to understand how I experienced my situation.",
-                                                              "The midwives/nurses/laboratory personnel were respectful towards me.",
-                                                              "The midwives/nurses/laboratory personnel showed commitment; ‘cared about me’.",
-                                                              "Themidwives/nurses/laboratory personnel seemedto understand how I experienced my situation."))
+dimensao3.selectbox('Informações durante o tratamento',("Recebi os melhores cuidados médicos possíveis (exames e tratamentos), tanto quanto posso dizer",
+                                                   "Recebi boas informações sobre os medicamentos de que precisava, para entender seus efeitos e como devem ser administrados.",
+                                                    "Recebi boas informações sobre os resultados dos exames (por exemplo, ultrassom, análises hormonais e de esperma)."))
+dimensao3.selectbox('Informações após o tratamento',("Recebi boas informações sobre a fertilização e o desenvolvimento do embrião no momento da transferência do embrião.",
+                                                   "Recebi boas informações sobre o tempo entre a transferência do embrião e o teste de gravidez."))
+dimensao3.selectbox('Participação',("Tive boas oportunidades de participar das decisões que se aplicavam ao meu tratamento.",
+                                     "Meu atendimento foi determinado por minhas próprias necessidades de solicitações, e não pelos procedimentos da equipe."))
+dimensao3.selectbox('Responsabilidade/continuidade',("Recebi boas informações sobre qual médico era o responsável pelo meu tratamento.",
+                                                 "Recebi boas informações sobre qual parteira era responsável pelo meu tratamento.",
+                                                 "Encontrei o mesmo médico em exames e tratamento durante esse período de tratamento.",
+                                                 "Conheci a mesma parteira em exames e entrevistas durante esse período de tratamento."))
+dimensao3.selectbox('Respeito/comprometimento/empatia da Equipe',("Os médicos foram respeitosos comigo.",
+                                                              "Os médicos demonstraram comprometimento; ‘se importavam comigo’.",
+                                                              "Os médicos pareciam entender como estava minha situação.",
+                                                              "As parteiras/enfermeiras/pessoal do laboratório foram respeitosas comigo.",
+                                                              "As parteiras/enfermeiras/pessoal do laboratório demonstraram comprometimento; ‘se importavam comigo’.",
+                                                              "As parteiras/enfermeiras/pessoal do laboratório pareciam entender como eu vivenciava minha situação."))
 dimensao4 = st.container()
 dimensao4.write('Dimensão 4 Atmosfera sócio-cultural')
-dimensao4.selectbox('Atmosphere and environment',("I received the best possible medical care (examinations and treatments) as far as I can tell"))
+dimensao4.selectbox('Atmosfera e ambiente',("Havia um ambiente agradável na clínica",
+                                            "Meu parceiro foi bem tratado.",
+                                            "Fiz exames e entrevistas em privado sem perturbações."))
